93967a84bfc8875d42e7753efee4ed09b73b89bd
mishimumu <793978044@qq.com>
2018-06-06 17:27:59 +0800

    Update SliderPageButton.cs

--- SliderPageButton.cs
+++ SliderPageButton.cs
@@ -1,72 +1,68 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using System;
-namespace UScroll
+using UScroll;
+public class SliderPageButton : MonoBehaviour
 {
-    public class SliderPageButton : Button, IDropHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    [SerializeField]
+    private int index;
+    private SliderPage page;
+    [HideInInspector]
+    public Button uBtn;
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private SliderPageOutScrollView view;
+
+    void Awake()
     {
-
-        public SliderPageControl view;
-
-        protected override void Start()
+        uBtn = GetComponent<Button>();
+        if (uBtn != null)
         {
-            base.Start();
-            view = GetComponentInParent<SliderPageControl>();
-            if (!view)
-            {
-                Debug.LogError("SliderPageControl is no exit");
-            }
-        }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-            Debug.Log("OnDrag");
-            view.scrollRect.OnDrag(eventData);
-
-        }
-
-
-        public override void OnPointerExit(PointerEventData eventData)
-        {
-            Debug.Log("OnPointerExit");
-        }
-
-        public void OnDrop(PointerEventData eventData)
-        {
-            Debug.Log("OnDrop");
-            view.OnPointerUp(eventData);
-        }
-
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            Debug.Log("OnBeginDrag");
-            view.OnPointerDown(eventData);
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            Debug.Log("OnEndDrag");
-            view.OnPointerUp(eventData);
-        }
-
-        public override void OnPointerDown(PointerEventData eventData)
-        {
-            Debug.Log("OnPointerDown");
-            view.OnPointerDown(eventData);
-        }
-
-
-        public override void OnPointerUp(PointerEventData eventData)
-        {
-            Debug.Log("OnPointerUp");
-        }
-
-        public override void OnSubmit(BaseEventData eventData)
-        {
-            Debug.Log("OnSubmit");
+            uBtn.onClick.AddListener(Onclick);
         }
     }
 
+    public void Init(SliderPage page)
+    {
+        if (view != null && view.pages.Count > index && page != null)
+        {
+            this.page = page;
+            view.FreshEvent += Refresh;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (view != null)
+        {
+            view.FreshEvent -= Refresh;
+        }
+    }
+
+    void Onclick()
+    {
+        if (page != null && view != null)
+        {
+            SliderPageScrollView.Refresh();
+            view.horizontalScrollbar.value = page.bar;
+            view.Fresh();
+        }
+    }
+
+    private void Refresh(float value)
+    {
+        Color color = icon.color;
+        if (page.min <= value && value <= page.max)
+        {
+            float per = 1 - Mathf.Abs(value - page.bar) / page.length;
+            color.a = per;
+        }
+        else
+        {
+            color.a = 0;
+        }
+        icon.color = color;
+    }
 }
